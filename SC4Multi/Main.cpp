1aba58c900eb4215675f9c8189350b160e979c27
storm3us <storm3us@gmail.com>
2012-02-26 11:36:13 -0500

    MEMORY

--- SC4Multi/Main.cpp
+++ SC4Multi/Main.cpp
@@ -1,16 +1,52 @@
 #include "Shared.h"
 
-BOOL WINAPI DllMain (HANDLE hDll, DWORD dwReason, LPVOID lpReserved)
+// www.gamersoul.com/forums/showthread.php?15873-TUT-C-Form-in-DLL-read-pointers
+__inline ULONG_PTR ReadPointer(ULONG_PTR* ulbase, int noffset)
 {
-    switch (dwReason)
-    {
-        case DLL_PROCESS_ATTACH:
-			MessageBoxA( NULL, "This is a throwdown.", "Injected DLL", NULL );
-            break;
+	if (!IsBadReadPtr((void*)ulbase, sizeof(ULONG_PTR)))
+	{
+		if (!IsBadReadPtr((void*)((*(ULONG_PTR*)ulbase)+noffset), sizeof(ULONG_PTR)))
+			return *(ULONG_PTR*)((*(ULONG_PTR*)ulbase)+noffset);
+	}
 
-        case DLL_PROCESS_DETACH:
-			MessageBoxA( NULL, "Detached -- I'm free!", "Injected DLL", NULL );
-            break;
-    }
+	return 0;
+}
+
+void lal()
+{
+	int cash    = (int)ReadPointer( (ULONG_PTR*)0x00b4418c, 0x00000028 );
+	char buf[35];
+
+	sprintf( buf, "%i", cash );
+	MessageBoxA( NULL, buf, "SC4Multi", NULL );
+}
+
+// www.infinityhacks.com/forum/showthread.php?7792-Most-basic-C-hack
+DWORD WINAPI entryPoint( LPVOID )
+{
+	while( GetModuleHandleA( "SC4Multi.dll" ) == NULL )
+		Sleep( 150 );
+
+	for( ;; )
+	{
+		if( GetAsyncKeyState( 0x4C ) ) // L key
+			lal();
+
+		Sleep( 100 );
+	}
+}
+
+BOOL WINAPI DllMain (HMODULE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+    DisableThreadLibraryCalls( hDll );
+	if( dwReason == DLL_PROCESS_ATTACH )
+	{
+		#ifdef DEBUG
+			MessageBoxA( NULL, "SC4Multi by stormeus, formerly simmaster07", "SC4Multi DLL", NULL );
+		#endif
+
+		CreateThread( NULL, NULL, entryPoint, NULL, NULL, NULL );
+	}
+
     return TRUE;
 }
--- SC4Multi/Shared.h
+++ SC4Multi/Shared.h
@@ -1,2 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Pretty cool macro, bro
+#define MakePtr(cast, ptr, addValue) (cast)( (DWORD)(ptr)+(DWORD)(addValue))
+
+// Aw yeah definitions
+#define DEBUG
